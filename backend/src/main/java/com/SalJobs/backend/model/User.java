5f59bbd6174d9052ba08911c13e8dbcf3942908f
Kenneth Velasquez <kgvelasq@usc.edu>
2022-05-02 15:22:53 -0700

    user class

--- backend/src/main/java/com/SalJobs/backend/model/User.java
+++ backend/src/main/java/com/SalJobs/backend/model/User.java
@@ -32,6 +32,53 @@ public class User {
     
     @Column(name = "authToken")
     private String authToken;
-
     
+    public User(long id, String email, String name, String password, String authToken) {
+    	this.id = id;
+    	this.email = email;
+    	this.name = name;
+    	this.password = password;
+    	this.authToken = authToken;
+    }
+    
+    public long getId() {
+    	return id;
+    }
+    
+    public void setId(long id) {
+    	this.id = id;
+    }
+    
+    public String getEmail() {
+    	return email;
+    }
+    
+    public void setEmail(String email) {
+    	this.email = email;
+    }
+    
+    public String getName() {
+    	return name;
+    }
+    
+    public void setName(String name) {
+    	this.name = name;
+    }
+    
+    public String getPassword() {
+    	return password;
+    }
+    
+    public void setPassword(String password) {
+    	this.password = password;
+    }
+    
+    public String getAuthToken() {
+    	return authToken;
+    }
+   
+    public void setAuthToken(String authToken) {
+    	this.authToken = authToken;
+    }
+   
 }
